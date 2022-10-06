f7b1a6be7cb3306e05ebf7c01b93e247aab60b6a
jump3rhood <mjm13121@gmail.com>
2022-10-06 11:33:58 +0530

    ex 3.7 step 7 configure morgan middleware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express();
 
-app.use(express.json()); // to parse json sent in the request
 
+app.use(express.json()); // to parse json sent in the request
+app.use(morgan('tiny'));
 let persons = [
     { 
         "id": 1,
