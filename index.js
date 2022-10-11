e11e2f433a3b7453365256131a0c6845d6f41619
jump3rhood <mjm13121@gmail.com>
2022-10-11 23:20:39 +0530

    ex3.17*: Phonebook database, step5 (PUT route added)

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.post('/api/persons', (request, response)=> {
+app.post('/api/persons', (request, response, next)=> {
     const person = request.body;
     if(!person.name || !person.number){
         return response.status(400).json({error: 'name and/or number missing'})
@@ -38,7 +38,7 @@ app.post('/api/persons', (request, response)=> {
     })
 })
 
-app.get('/api/persons/:id', (request, response)=> {
+app.get('/api/persons/:id', (request, response, next)=> {
     Person.findById(request.params.id)
         .then(person => {
             if(person)
@@ -49,7 +49,16 @@ app.get('/api/persons/:id', (request, response)=> {
         next(error)
     })
 })
-app.delete('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request,response, next) => {
+    const id = request.params.id;
+    const body = request.body;
+   
+    Person.findByIdAndUpdate(id, body, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson);
+    }).catch(error => next(error));
+})
+app.delete('/api/persons/:id', (request, response, next) => {
     const id = request.params.id;
     Person.findByIdAndRemove(id)
         .then(result => {
