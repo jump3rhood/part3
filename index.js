ddec2c6f5eeccaab472fe9d1f4fab9f0d57bc3cd
jump3rhood <mjm13121@gmail.com>
2022-10-11 23:04:49 +0530

    ex3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -5,39 +5,18 @@ const cors = require('cors');
 const app = express();
 const Person = require('./models/person')
 const logger = morgan(':method :url :status :res[content-length] - :response-time ms :body');
+app.use(express.static('build'))
 // allow cors
 app.use(cors());
 app.use(express.json()); // to parse json sent in the request
 app.use(logger);    // logger
-app.use(express.static('build'))
 morgan.token('body', (req) => {
     return JSON.stringify(req.body);
 })
-let persons = [
-    { 
-        "id": 1,
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": 2,
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": 3,
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": 4,
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-      }
-];
+
 app.get('/info', (request, response)=> {
     const date = new Date();
-    response.send(`Phonebook has info for ${persons.length} people
+    response.send(`Phonebook has info for people
         ${date.toString()}`)
 })
 app.get('/api/persons', (request, response) => {
@@ -54,18 +33,33 @@ app.post('/api/persons', (request, response)=> {
     const newPerson = new Person(person);
     newPerson.save().then(person => {
         response.status(200).json(person);
-    }).catch(err => response.status(400).json({error: err.message}))
+    }).catch(err => {
+        response.status(400).json({error: err.message})
+    })
 })
 
 app.get('/api/persons/:id', (request, response)=> {
-    Person.findById(request.params.id).then(person => {
-        response.status(200).json(person);
-    }).catch(err => response.status(404).end())
+    Person.findById(request.params.id)
+        .then(person => {
+            if(person)
+                response.status(200).json(person);
+            else 
+                response.status(400).end()
+    }).catch(err => {
+        console.log(error)
+        response.status(400).send({ error: 'malformatted id' })
+    })
 })
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    persons = persons.filter(p => p.id !== id);
-    response.status(204).end();
+    const id = request.params.id;
+    Person.findByIdAndRemove(id)
+        .then(result => {
+            console.log(result);
+            response.status(202).end();
+        }).catch(error => {
+            console.log(error);
+            response.status(500).end();
+        })
 })
 const generateId = () => {
     return Math.floor(Math.random()*1000+1);
